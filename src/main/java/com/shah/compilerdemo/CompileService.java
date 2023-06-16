d15c9335635cd75bcfb03ddd0f526294a6c8ba6b
Sohailshah20 <contact.sohailali@gmail.com>
2023-06-16 15:31:14 +0530

    Delete files feature added and fine improvements

--- src/main/java/com/shah/compilerdemo/CompileService.java
+++ src/main/java/com/shah/compilerdemo/CompileService.java
@@ -8,6 +8,7 @@ import com.github.dockerjava.api.command.WaitContainerResultCallback;
 import com.github.dockerjava.api.model.*;
 import com.github.dockerjava.core.command.LogContainerResultCallback;
 import org.springframework.stereotype.Service;
+
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
@@ -18,11 +19,11 @@ import java.util.List;
 @Service
 public class CompileService {
 
-    private DockerClient dockerClient;
+    private final DockerClient dockerClient;
 
     private final String imageId = "a3562aa0b991";
-    private String javaFilePath = "E:/temp/";
-    private String classPath = "E:/temp/Hello.class";
+    private final String javaFilePath = "E:/temp/";
+    private final String classPath = "E:/temp/Hello.class";
 
     public CompileService(DockerClient dockerClient) {
         this.dockerClient = dockerClient;
@@ -31,7 +32,6 @@ public class CompileService {
     public List<String> runCommandInContainer() {
         ListImagesCmd images = dockerClient.listImagesCmd();
         List<Image> exec = images.exec();
-        Image image = exec.get(0);
         List<String> imageNames = new ArrayList<>();
         for (int i = 0; i < exec.size(); i++) {
             for (String tag : exec.get(i).getRepoTags()) {
@@ -48,12 +48,11 @@ public class CompileService {
         fullcode.append("}");
         String fileName = "Hello.java";
         File newFile = new File("E:/temp/Hello.java");
-        try  {
-            if (!newFile.exists()){
-                System.out.println("*********************************File is not created");
+        try {
+            if (!newFile.exists()) {
                 newFile.createNewFile();
             }
-            System.out.println("*********************************" + fullcode.toString());
+            System.out.println("*********************************" + fullcode);
             FileWriter writer = new FileWriter(newFile);
             BufferedWriter bufferedWriter = new BufferedWriter(writer);
 
@@ -67,7 +66,6 @@ public class CompileService {
     }
 
     public String compileCode(String command) {
-        StringBuilder error = new StringBuilder();
         StringBuilder stdout = new StringBuilder();
         StringBuilder stderr = new StringBuilder();
         try {
@@ -85,12 +83,17 @@ public class CompileService {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        if (error.toString().isEmpty()) {
+        if (stderr.toString().isEmpty()) {
+            System.out.println("*******************ERROR MESSAGE STRING " + stderr.toString());
             return runProgram("java Hello");
-        }else return error.toString();
+        } else {
+            deleteFile(javaFilePath, "Hello.java", null);
+            System.out.println("*******************RETURNING COMPILE ERROR");
+            return stderr.toString();
+        }
     }
 
-    public String runProgram(String command){
+    public String runProgram(String command) {
         System.out.println("*******************IN THE RUN METHOD");
         StringBuilder stdout = new StringBuilder();
         StringBuilder stderr = new StringBuilder();
@@ -109,7 +112,13 @@ public class CompileService {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        return !stderr.toString().isEmpty() ? stderr.toString() : stdout.toString();
+        if (!stderr.toString().isEmpty()) {
+            deleteFile(javaFilePath, "Hello.java", "Hello.class");
+            return stderr.toString();
+        } else {
+            deleteFile(javaFilePath, "Hello.java", "Hello.class");
+            return stdout.toString();
+        }
     }
 
     private void removeContainer(CreateContainerResponse sh) {
@@ -135,27 +144,43 @@ public class CompileService {
     }
 
 
-
     private void waitForContainerExecution(CreateContainerResponse sh) throws InterruptedException {
         dockerClient.waitContainerCmd(sh.getId())
                 .exec(new WaitContainerResultCallback())
                 .awaitCompletion();
     }
 
-    private static LogContainerResultCallback getLogContainerResultCallback(StringBuilder stdout, StringBuilder stderr) {
+    private static LogContainerResultCallback getLogContainerResultCallback(StringBuilder stdout, StringBuilder
+            stderr) {
         LogContainerResultCallback logCallback = new LogContainerResultCallback() {
             @Override
             public void onNext(Frame item) {
                 if (item.getStreamType().equals(StreamType.STDOUT)) {
                     stdout.append(new String(item.getPayload()));
-                    System.out.print("******************  STDOUT" +new String(item.getPayload()));
+                    System.out.print("******************  STDOUT" + new String(item.getPayload()));
                 } else if (item.getStreamType().equals(StreamType.STDERR)) {
                     stderr.append(new String(item.getPayload()));
-                    System.err.print("******************  ERROR" +new String(item.getPayload()));
+                    System.err.print("******************  ERROR" + new String(item.getPayload()));
                 }
             }
         };
         return logCallback;
     }
 
+    private void deleteFile(String filePath, String filename, String compiledFileName) {
+        File file1 = new File(filePath, filename);
+
+        if (file1.exists()) {
+            boolean delete = file1.delete();
+            if (delete) System.out.println("***************************** FILE DELETED");
+        }
+        if (compiledFileName != null) {
+            File file2 = new File(filePath, compiledFileName);
+            System.out.println("***************************** FILE 2" + file2);
+            if (file2.exists()) {
+                boolean delete = file2.delete();
+                if (delete) System.out.println("***************************** COMPILED FILE DELETED");
+            }
+        }
+    }
 }
